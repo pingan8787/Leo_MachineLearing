cd844aa1a43f05186fda182e911e04a819dfe611
王平安 <pingan8787@qq.com>
2017-11-29 07:21:25 +0800

    2017.11.29

--- 1-Python/(1)常用高阶函数demo.py
+++ 1-Python/(1)常用高阶函数demo.py
@@ -1,6 +1,6 @@
 
 #coding:utf-8
-# 1/高阶函数 map()
+# 1、高阶函数 map()
 ## 为list每个值执行相同函数操作,只有一个参数
 def f(x):
     return x*x
@@ -12,38 +12,70 @@ def format_name(name):
     return name[:1].upper() + name[1:].lower()
 print map(format_name,['leo','lEo','leO'])
 
-# 2/高阶函数 reduce()
+# 2、高阶函数 reduce()
 ## 为list每个值执行相同函数操作,最少两个参数
-def g(x,y):
+## reduce把结果继续和序列的下一个元素做 累积计算，如本次这个和下次一个一起算
+from functools import reduce
+def g(x,y):                            # 求和函数
     return x+y
-print reduce(g,[1,2,3,4,5,6])
+print reduce(g,[1,2,3,4,5,6])          
+## 求和运算可以直接用Python内建函数sum()，没必要动用reduce。
 
-def prod(x,y):      # 求积函数
+from functools import reduce
+def prod(x,y):                         # 求积函数
     return x*y
 print reduce(prod,[1,2,3,4,5,6],100)   # 第三个参数为初始值
 
-# 3/高阶函数 filter()
-## 为list每个值执行过滤操作,只要一个参数
-def is_odd(x):      # 过滤list中的偶数，保留奇数
+## 案例
+## 输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
+from functools import reduce
+def normalize(name):
+    return name[:1].upper()+name[1:].lower();
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+# 3、高阶函数 filter()
+## 为list每个值执行过滤操作,只要一个参数，filter()函数用于过滤序列。
+def is_odd(x):          # 过滤list中的偶数，保留奇数
     return x%2==1
 print filter(is_odd,[1,2,3,4,5,6])
 
-def is_empty(x):    # 过滤list中的空值，保留存在值
+def is_empty(x):        # 过滤list中的空值，保留存在值
     return x and len(x.strip())>0
 print filter(is_empty,['leo',None,'123',' ','','END'])
 
 import math
-def is_sqr(x):     # 过滤1-100中平方根是整数的数
+def is_sqr(x):          # 过滤1-100中平方根是整数的数
     sqr = int(math.sqrt(x))
     return sqr*sqr == x
 print filter(is_sqr,range(1,101))
 
-# 4/高阶函数 sorted()
+def primes():           # 一个计算100以内素数的函数
+    yield 2
+    it = _odd_iter()    # 初始序列
+    while True:
+        n = next(it)    # 返回序列的第一个数
+        yield n
+        it = filter(_odd_iter(n),it)  # 构造新序列
+for n in primes():      # 由于primes()是一个无限序列，所以调用时需设置一个退出循环的条件：
+    if n <1000:
+        print(n)
+    else:
+        break 
+
+# 4、高阶函数 sorted()
 ## 自定义排序，x在前y在后 返回-1，反之返回1，相同返回0
 test_sort = [36, 5, 12, 9, 21]
 string_sort = ['bob', 'about', 'Zoo', 'Credit']
+abs_sort = [36, 5, -12, 9, -21]
 print sorted(test_sort)
 print sorted(string_sort)
+## sorted()函数是一个高阶函数，还可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序
+print sorted(abs_sort,key = abs)
+print sorted(string_sort,key = str.lower)
+## 要进行反向排序，不必改动key函数，可以传入第三个参数reverse=True
+print sorted(string_sort,key = str.lower, reverse=True)
 
 def is_cmp(x,y):
     if x>y:
