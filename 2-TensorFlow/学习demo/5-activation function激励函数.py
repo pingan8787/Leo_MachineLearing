44baeba23a800daa85b006b8c9b23a38c7142d16
王平安 <pingan8787@qq.com>
2017-12-11 23:43:50 +0800

    2017.12.11

---
+++ 2-TensorFlow/学习demo/5-activation function激励函数.py
@@ -0,0 +1,5 @@
+### demo 激励函数  ###
+## 激励函数：让某一部分的函数先激活起来，再将激活效应的信息传递到后面函数 ##
+## 激励函数：实际上就是方程 ##
+## 常见激活函数 如tf.nn.relu() 参考文档里面具体 ##
+## 文档地址 http://www.tensorfly.cn/tfdoc/api_docs/python/nn.html  ##
