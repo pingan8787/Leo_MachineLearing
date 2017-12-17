2df53c0f9244d8bc5b4dd22b773fdd078e903a8f
王平安 <pingan8787@qq.com>
2017-12-17 22:24:26 +0800

    2017.12.17

--- 1-Python/3-Numpy/3-利用数组进行数据处理.py
+++ 1-Python/3-Numpy/3-利用数组进行数据处理.py
@@ -13,3 +13,12 @@ plt.imshow(p2,cmap=plt.cm.gray);
 plt.colorbar()
 plt.title('第一个图，image plot of $\sqrt{x^2 + y^2}$ for a grid of values')
 plt.show()                   # 显示图片
+
+# 二、将条件逻辑表述为数组运算
+## numpy.where函数是三元表达式 x if condition else y 的矢量化版本。
+ax = np.array([1.1,1.2,1.3,1.4,1.5])
+ay = np.array([2.1,2.2,2.3,2.4,2.5])
+ac = np.array([True,False,True,True,False])
+## 实现 当ac中的值为True则取ax的值，反之去ay的值
+ar = np.where(ac,ax,ay)
+ar             # array([ 1.1,  2.2,  1.3,  1.4,  2.5])
