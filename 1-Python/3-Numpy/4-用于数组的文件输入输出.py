1a67460576a528b5152612a3f5ba8732b12ef1a5
王平安 <pingan8787@qq.com>
2017-12-18 23:13:50 +0800

    2017.12.18

--- 1-Python/3-Numpy/4-用于数组的文件输入输出.py
+++ 1-Python/3-Numpy/4-用于数组的文件输入输出.py
@@ -3,5 +3,9 @@
 ## 默认情况，数组是以未压缩的原始二进制格式保存在拓展名为.npy文件中。
 import numpy as np
 a1 = np.arange(10)
-np.save('test1',a1)           # 生成文件  即可在当前运行目录中生成 test1.npy 文件
-np.load('test1.npy')          # 读取文件
+np.save('test1',a1)             # 生成文件  即可在当前运行目录中生成 test1.npy 文件
+np.load('test1.npy')            # 读取文件  array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+## 通过np.savez可以将多个数组保存到一个压缩文件，将数组以关键字参数形式传入即可
+np.savez('test2.npz',a=a1,b=a1) # 生成文件
+a3 = np.load('test2.npz')       # 读取文件
+a3['a']                         # array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
