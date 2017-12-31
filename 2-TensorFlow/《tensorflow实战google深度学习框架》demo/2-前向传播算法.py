a68d12b91ae75afb313ceb48acbb391105a3b3d2
王平安 <pingan8787@qq.com>
2017-12-31 22:27:08 +0800

    2017.12.31

--- 2-TensorFlow/《tensorflow实战google深度学习框架》demo/2-前向传播算法.py
+++ 2-TensorFlow/《tensorflow实战google深度学习框架》demo/2-前向传播算法.py
@@ -19,3 +19,29 @@ sess.run(w2.initializer)
 # 获取y值
 print(sess.run(y))  # [[ 3.95757794]]
 sess.close()
+
+#############################################
+
+## 升级版  通过placeholder写法：
+import tensorflow as tf
+
+# 声明2个变量，并通过seed设置随机种子
+w1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+# xx 输入值的特征向量，暂时定为常量，为1*2矩阵
+# 若这里使用的是 n*m 矩阵，则在feed_dict也要传入对应格式数据
+x = tf.placeholder(tf.float32, shape=(1,2), name="input")
+
+# 通过前面描述的前向传播算法，获得神经网络的输出
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+sess = tf.Session()
+init_op = tf.global_variables_initializer()
+# initialize_all_variables被 global_variables_initializer 代替
+sess.run(init_op)
+
+# 获取y值
+print(sess.run(y,feed_dict={x:[[0.7,0.9]]}))  # [[ 3.95757794]]
+sess.close()
