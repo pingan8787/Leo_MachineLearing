7ac1e76de4a0aef625c86fcfb9b1ee9ce83e3ef3
王平安 <pingan8787@qq.com>
2018-01-02 22:54:30 +0800

    2018.01.02

--- 1-Python/4-《唐宇迪python数据分析》/1-Numpy/1-科学计算库Numpy.py
+++ 1-Python/4-《唐宇迪python数据分析》/1-Numpy/1-科学计算库Numpy.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# 1、读取文件数据
 pk10 = np.genfromtxt("../pk10.txt",delimiter="	",dtype="str")  # 读取文件数据，并且指定“	”为分隔符
 print(type(pk10))
 print(pk10)
@@ -14,3 +15,39 @@ print(help(np.genfromtxt))   # 查看函数的帮助文档
  ['2012-01-01' '274574' '2,5,4,7,6,10,8,1,9,3']
  ['2012-01-01' '274573' '8,4,6,3,2,9,5,10,1,7']]
 '''
+
+# 2、创建矩阵
+a1 = np.array([1,2,3])             # 创建一维矩阵
+a2 = np.array([[1,2,3],[1,2,3]])   # 创建一个二维矩阵
+print(a1.shape)                   # 查看矩阵的结构 (3,)  3行1列
+print(a2.shape)                   # 查看矩阵的结构 (2,3) 2行3列
+
+# 3、类型转换
+## numpy array传入的值必须同一类型，不然会自动转换成为较少数量的数值类型
+b1 = np.array([1,2,3,4])
+b2 = np.array([1,2,3,4.])
+b3 = np.array([1,2,3,'4'])
+print(b1.dtype)                   # int32
+print(b2.dtype)                   # float64
+print(b3.dtype)                   # <U11
+
+# 4、选取数据
+## 和python使用方法一样
+c1 = np.genfromtxt("../pk10.txt",delimiter="	",dtype="str")
+print(c1[1,2])                    # 5,9,10,4,2,7,3,6,8,1
+print(c1[2,1])                    # 656196
+
+# 5、切片
+d1 = np.array([5,10,15,20])
+print(d1[0:3])                    #  [5,10,15] 切片切取0到3的数据 不包括3
+d2 = np.array([
+    [5,10,15],
+    [20,25,30],
+    [30,35,40]
+])
+print(d2[:,1])                    # [10 25 35] 切片切取整个数据结构的下标为1的列
+
+
+
+
+
