09fee2a4947722518bbf636ab982f479bdbaff8c
王平安 <pingan8787@qq.com>
2018-01-13 21:23:27 +0800

    2018.01.13

--- 1-Python/4-《唐宇迪python数据分析》/3-Matplotlib/2-图像绘制工具Matplotlib[柱状图等].py
+++ 1-Python/4-《唐宇迪python数据分析》/3-Matplotlib/2-图像绘制工具Matplotlib[柱状图等].py
@@ -1,5 +1,4 @@
-# 1、绘制简单柱状图
-
+# 0、通用代码
 import pandas as pd
 from numpy import arange
 import matplotlib.pyplot as plt
@@ -9,8 +8,28 @@ num_cols = ["RT_user_norm","Metacritic_user_nom","IMDB_norm","Fandango_Ratingval
 norm_reviews = reviews[num_cols]
 print (norm_reviews[:1])
 
+## 设置中文乱码问题
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+
+# 1、绘制简单柱状图
+# bar_heights = norm_reviews.ix[0,num_cols].values      ## 每个柱的高度
+# bar_positions = arange(5) + 0.75                      ## 每个柱离 0 值的距离
+# fig,ax= plt.subplots()                                ## 把图拿出来
+# ax.bar(bar_positions,bar_heights,0.5)                 # ax.bar( 离0点横坐标 , 柱高度 , 柱宽度)
+# plt.show()
+
+# 2、绘制带有title和label的柱状图
 bar_heights = norm_reviews.ix[0,num_cols].values      ## 每个柱的高度
-bar_positions = arange(5) + 0.75                      ## 每个柱离 0 值的距离
-fig,ax= plt.subplots()                                ## 把图拿出来
-ax.bar(bar_positions,bar_heights,0.5)                 # ax.bar( 离0点横坐标 , 柱高度 , 柱宽度)
+bar_positions = arange(5) + 0.75
+tick_positions = range(1,6)
+fig,ax = plt.subplots()
+
+ax.bar(bar_positions, bar_heights, 0.5)
+ax.set_xticks(tick_positions)                         # 设置 x 轴刻度值
+ax.set_xticklabels(num_cols,rotation=45)              # 设置 x 轴刻度值的旋转角度
+ax.set_xlabel("Rating Source")                     # 设置 x 轴名称
+ax.set_ylabel("Average Rating")                    # 设置 y 轴名称
+ax.set_title("这是标题")                             # 设置 图表 标题
 plt.show()
+
