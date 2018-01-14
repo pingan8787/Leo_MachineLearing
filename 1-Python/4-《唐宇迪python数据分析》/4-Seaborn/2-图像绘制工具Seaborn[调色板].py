a2ab1ce732c63c4630686cac411fcfe4c3d334fe
王平安 <pingan8787@qq.com>
2018-01-14 22:22:58 +0800

    2018.01.14

--- 1-Python/4-《唐宇迪python数据分析》/4-Seaborn/2-图像绘制工具Seaborn[调色板].py
+++ 1-Python/4-《唐宇迪python数据分析》/4-Seaborn/2-图像绘制工具Seaborn[调色板].py
@@ -15,7 +15,7 @@ current_palette = sns.color_palette()    # 没有传入颜色 则使用默认
 sns.palplot(current_palette)
 plt.show()
 
-## 3、圆形画板
+## 2、圆形画板
 ### 即在一个圆形的颜色空间中均匀间隔的分布颜色 最常用的是hls的颜色空间，这是RGB值的一个简单转换
 sns.palplot(sns.color_palette("hls", 8))  # 均匀平分成8份
 plt.show()
@@ -30,5 +30,48 @@ plt.show()
 sns.palplot(sns.hls_palette(8, l=.3, s=.8))  # hls_palette( 颜色数量，亮度，饱和度)
 plt.show()
 
-sns.palplot(sns.color_palette("Paired",10))
+sns.palplot(sns.color_palette("Paired",10)) # Paired 类型表示颜色成对出现，这里出现5对10个
+plt.show()
+
+### 使用skcd颜色来命名颜色
+### 用得少 查官网来使用
+plt.plot([0, 1],[0, 1],sns.xkcd_rgb["pale_red"], lw = 3)
+plt.plot([0, 1],[0, 2],sns.xkcd_rgb["medium_green"], lw = 3)
+plt.plot([0, 1],[0, 3],sns.xkcd_rgb["denim_blue"], lw = 3)
+plt.show()
+
+## 3、连续画板
+sns.palplot(sns.color_palette("Blues"))
+plt.show()
+
+sns.palplot(sns.color_palette("BuGn_r"))  # 颜色名加 " _r " 表示翻转渐变色
+plt.show()
+
+## 4、cubehelix_palette() 调色板
+### 色调线性变换
+sns.palplot(sns.color_palette("cubehelix", 8))
+plt.show()
+
+sns.palplot(sns.cubehelix_palette(8, start=.5, rot=-0.75))  # start rot 指定颜色区间
+plt.show()
+
+sns.palplot(sns.cubehelix_palette(8, start=.75, rot=-.150))
+plt.show()
+
+## 5、light_palette() 和 dark_palette 调用定制连续调色板
+sns.palplot(sns.light_palette("green"))
+plt.show()
+
+sns.palplot(sns.light_palette("purple"))
+plt.show()
+
+sns.palplot(sns.light_palette("navy", reverse=True))  # reverse 倒序排序颜色
+plt.show()
+
+x, y = np.random.multivariate_normal([0, 0],[[1,-.5],[-.5,1]],size=300).T
+pal = sns.dark_palette("green",as_cmap=True)
+sns.kdeplot(x,y,cmap=pal)
+plt.show()
+
+sns.palplot(sns.light_palette((210,90,60)),input="husl") # input 指定颜色空间 基本不用
 plt.show()
